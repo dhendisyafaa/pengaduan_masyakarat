e9abe61f61f5a28edf1860dbd087e06af8d11584
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-08-22 09:51:17 +0700

    feat: change controller method name

--- routes/pengaduan.routes.js
+++ routes/pengaduan.routes.js
@@ -3,16 +3,16 @@ import {
   createPengaduan,
   deletePengaduan,
   editPengaduan,
-  getAllPengaduans,
+  getAllPengaduan,
   getPengaduanById,
 } from "../controllers/pengaduan.controller.js";
 
 const router = express.Router();
 
-router.get("/pengaduans", getAllPengaduans);
-router.get("/pengaduan:id", getPengaduanById);
+router.get("/pengaduan", getAllPengaduan);
+router.get("/pengaduan/:id", getPengaduanById);
 router.post("/pengaduan", createPengaduan);
-router.patch("/pengaduan:id", editPengaduan);
-router.delete("/pengaduan:id", deletePengaduan);
+router.patch("/pengaduan/:id", editPengaduan);
+router.delete("/pengaduan/:id", deletePengaduan);
 
 export default router;
