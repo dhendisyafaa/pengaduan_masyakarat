982838daaff763c5faee4c53d07caed28733365d
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-08-22 09:53:28 +0700

    feat: add routes, static img

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import fileUpload from "express-fileupload";
 import MasyarakatRoutes from "./routes/masyarakat.routes.js";
 import PengaduanRoutes from "./routes/pengaduan.routes.js";
+import PetugasRoutes from "./routes/petugas.routes.js";
+import TanggapanRoutes from "./routes/tanggapan.routes.js";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.APP_PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
 
 // routes
 app.use(MasyarakatRoutes);
 app.use(PengaduanRoutes);
+app.use(PetugasRoutes);
+app.use(TanggapanRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}...`);
